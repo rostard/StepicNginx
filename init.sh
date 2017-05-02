f697702597a3795baddafbb31c71988f4a8833c2
rostard-ub <kos.boykow@gmail.com>
2017-05-02 15:08:50 +0300

    Add directories and test nginx.conf

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+
