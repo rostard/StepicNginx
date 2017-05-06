befae1772f86d479ffd81e197856ef5864705437
rostard-ub <kos.boykow@gmail.com>
2017-05-06 11:09:16 +0300

    Edit hello

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
-def application (env, start_response):
+def application (environ, start_response):
 	status='200 OK'
 	headers=[('Content-type','text/plain')]
 
-    body=env['QUERY_STRING'].split('&')
+    body=environ['QUERY_STRING'].split('&')
+
 	
 
 	start_response(status,headers)
