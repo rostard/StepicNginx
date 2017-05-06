7b57b76ba6dea5ff3dad1b03ed890b420b708306
rostard-ub <kos.boykow@gmail.com>
2017-05-06 10:51:00 +0300

    Edit init

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo gunicorn hello.py
-sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
+sudo /etc/init.d/nginx restart
+sudo gunicorn -b ['0.0.0.0:8080'] hello.py
