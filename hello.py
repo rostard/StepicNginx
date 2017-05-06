9e3a76c86f4176aa49a61125e48242e6a6d45392
rostard-ub <kos.boykow@gmail.com>
2017-05-06 11:30:56 +0300

    tabs

--- hello.py
+++ hello.py
@@ -2,9 +2,9 @@ def application (environ, start_response):
 	status='200 OK'
 	headers=[('Content-type','text/plain')]
 
-    body=environ['QUERY_STRING'].split('&')
+	body=environ['QUERY_STRING'].split('&')
 
-	
+
 
 	start_response(status,headers)
 	return body
