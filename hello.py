714fdbaf596a40343cf5ceff12e9a2359e980563
rostard-ub <kos.boykow@gmail.com>
2017-05-06 12:02:15 +0300

    Next commit

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 def application (environ, start_response):
 	status='200 OK'
 	headers=[('Content-type','text/plain')]
-
-	body=environ['QUERY_STRING'].split('&')
-
+	
+	with open("log.txt","w") as onf:
+		onf.write(environ['QUERY_STRING'])
+		body=environ['QUERY_STRING'].split('&')
+		onf.write(str(body))
 
 
 	start_response(status,headers)
--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello.py
+sudo gunicorn -b 0.0.0.0:8080 -D hello.py
