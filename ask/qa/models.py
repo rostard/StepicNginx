7ced8f082ea67f4fd9127351324f70515969bdcd
rostart <kos.boykow@gmail.com>
2017-06-02 07:07:54 +0000

    add question model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+	title = models.CharField (max_length=255)
+	text = models.TextField()
+	added_at = models.DateField()
+	rating = models.IntegerField()
+	author = ForeignKey(User)
+	likes = ManyToManyField(User)
