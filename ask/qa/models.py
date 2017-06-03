18a5f5f407bd7ce31c44ab1f15dc370b31d3166f
rostard-ub <kos.boykow@gmail.com>
2017-06-03 11:15:18 +0300

    Try fix

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+	def new(self):
+		pass
+	def popular(self):
+		pass
+
 class Question(models.Model):
 	objects=QuestionManager()
 	title = models.CharField (max_length=255)
@@ -9,16 +15,12 @@ class Question(models.Model):
 	added_at = models.DateField()
 	rating = models.IntegerField()
 	author = ForeignKey(User)
-	likes = ManyToManyField(User)
+	likes = ManyToManyField(User, null=True, on_delete=models.SET_NULL)
 
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateField()
-	question = ForeignKey(Question)
-	author = ForeignKey(User)
+	question = ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+	author = ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-class QuestionManager(models.Manager):
-	def new(self):
-		pass
-	def popular(self):
-		pass
+
